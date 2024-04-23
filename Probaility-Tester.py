e698cb07b3382bd911f818f2a3f8af3efdc85309
Mert Çakır <82875825+MERT-CKR@users.noreply.github.com>
2024-04-24 02:25:03 +0300

    Add files via upload

--- Probaility-Tester.py
+++ Probaility-Tester.py
@@ -1,32 +1,30 @@
 import random
-import time
 
-print("Tahmin etmeye çalıştığınız olasılık yüzdesi nedir? Örn: 50\n")#sample: 50
-targetProbability = int(input(">> "))
-print("\n")
+while True:
+    print("Tahmin etmeye çalıştığınız olasılık yüzdesi nedir? Örn: 50\n")#sample: 50
+    targetProbability = int(input(">> "))
+    print("\n")
 
-print("kaç kez denemek istersin ? \nDenk gelene kadar denemek için 0: ")
-limit = int(input(">> "))
-print("\n")
+    print("kaç kez denemek istersin ? \nDenk gelene kadar denemek için 0: ")
+    limit = int(input(">> "))
+    print("\n")
 
 
-unlimited = 0
-if limit == 0:
-    unlimited = 1
-    limit = 1
-counter = 0
+    unlimited = 0
+    if limit == 0:
+        unlimited = 1
+        limit = 1
+    counter = 0
 
-while limit > 0:
-    if unlimited != 1:
-        limit -= 1
-    input("Denemek için Eneter'e basın")
-    randomNumber = random.randint(1,100)
-    counter += 1
-    if randomNumber <= targetProbability:
-        print(f"{counter}. deneme Başarılı. ＼⁠(⁠^⁠o⁠^⁠)⁠／")
-        print("pencere 5 saniye sonra kapanıyor ")
-        time.sleep(5)
-        break
-    else:
-        print(f"{counter}. deneme Başarısız \n")
+    while limit > 0:
+        if unlimited != 1:
+            limit -= 1
+        input("Denemek için Enter'e basın")
+        randomNumber = random.randint(1,100)
+        counter += 1
+        if randomNumber <= targetProbability:
+            print(f"{counter}. deneme Başarılı :D")
+            break
+        else:
+            print(f"{counter}. deneme Başarısız \n")
     
